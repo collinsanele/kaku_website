0bf16e5b7cfeddaca854aeb1e15dba732fa9f868
Abdulhamid Usman <hazemidoo@gmail.com>
2020-12-05 11:43:29 +0100

    initial commit

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -10,6 +10,28 @@ const SecondPage = () => (
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
+    <div className="w-full bg-pink-500  lg:w-4/12 px-4 lg:order-1">
+                    <div className="flex justify-center py-4 lg:pt-4 pt-8">
+                      <div className="mr-4 p-3 text-center">
+                        <span className="text-xl font-bold block uppercase tracking-wide text-gray-700">
+                          22
+                        </span>
+                        <span className="text-sm text-gray-500">Friends</span>
+                      </div>
+                      <div className="mr-4 p-3 text-center">
+                        <span className="text-xl font-bold block uppercase tracking-wide text-gray-700">
+                          10
+                        </span>
+                        <span className="text-sm text-gray-500">Photos</span>
+                      </div>
+                      <div className="lg:mr-4 p-3 text-center">
+                        <span className="text-xl font-bold block uppercase tracking-wide text-gray-700">
+                          89
+                        </span>
+                        <span className="text-sm text-gray-500">Comments</span>
+                      </div>
+                    </div>
+                  </div>
   </Layout>
 )
 
