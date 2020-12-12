8bad082e5acdc68368f1234fbd5621d863c9b804
Abdul Hamid Usman <Hazemidoo@gmail.com>
2020-12-12 11:41:53 +0100

    sat morn

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ const IndexPage = () => {
     <div  className="h-12"></div>
     <Link  to="/page-2/">Go to page 2</Link> 
     
-    <h1>DUMMY CONTENT 1bbiuuhv</h1>
+    <h1>DUMMY CONTENT 1</h1>
 
     <p>Consectetur adipisicing elit. Tempore tempora rerum, est autem cupiditate, corporis a qui libero ipsum delectus quidem dolor at nulla, adipisci veniam in reiciendis aut asperiores omnis blanditiis quod quas laborum nam! Fuga ad tempora in aspernatur pariaturlores sunt esse magni, ut, dignissimos.</p>
     <p>Lorem ipsum cupiditate, corporis a qui libero ipsum delectus quidem dolor at nulla, adipisci veniam in reiciendis aut asperiores omnis blanditiis quod quas laborum nam! Fuga ad tempora in aspernatur pariatur fugit quibusdam dolores sunt esse magni, ut, dignissimos.</p>
