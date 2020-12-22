3709d23ce0b4ea3ece2c48eea459371f3a31f295
Abdul Hamid Usman <Hazemidoo@gmail.com>
2020-12-22 11:46:39 -0800

    purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme")
 
 module.exports = {
+  purge: ["./src/**/*.js", "./src/**/*.jsx", "./src/**/*.ts", "./src/**/*.tsx"],
   theme: {
     extend: {
       fontFamily: {
