72a8e1dda5825e53e64b00a41e9f732ddd4e2bae
Abdul Hamid Usman <Hazemidoo@gmail.com>
2020-12-22 12:12:32 -0800

    removed transparency

--- src/components/header.js
+++ src/components/header.js
@@ -8,34 +8,34 @@ import { Squeeze as Hamburger } from 'hamburger-react'
 const Header = ({ siteTitle }) => {
   const [menuOpen, setMenuOpen] = useState(false);
   return (
-  <header className="flex flex-wrap fixed w-full h-12 bgcolor lg:bg-blue-700 ">
-          <nav className=" flex flex-wrap items-center justify-between px-2 navbar-expand-lg w-full bg-transparent">
-            <div className="px-4 w-full flex flex-wrap items-center justify-between bg-transparent">
-              <div className="w-full relative flex justify-between lg:w-auto pr-4 lg:static lg:block lg:justify-start bg-transparent">
+  <header className="flex flex-wrap fixed w-full h-14 bgcolor">
+          <nav className=" flex flex-wrap items-center justify-between px-2 navbar-expand-lg w-fullt">
+            <div className="px-4 w-full flex flex-wrap items-center justify-betweent">
+              <div className="w-full relative flex justify-between lg:w-auto pr-4 lg:static lg:block lg:justify-start">
                 <Link
                   to="/"
-                  className="font-bold inline-block mr-4 py-2 whitespace-no-wrap uppercase text-blue bg-transparent pt-3"
+                  className="font-bold inline-block mr-4 py-2 whitespace-no-wrap uppercase text-blue pt-3"
                 >
                   {siteTitle}
                 </Link>
                 <button
-                  className="text-blue cursor-pointer px-3 bg-transparent block lg:hidden outline-none focus:outline-none relative left-10"
+                  className="text-blue cursor-pointer px-3 block lg:hidden outline-none focus:outline-none relative left-10"
                   type="button"
                   onClick={() => setMenuOpen(!menuOpen)}
                 >
-                  <Hamburger rounded size={16} className="bg-transparent"/>
+                  <Hamburger rounded size={16}/>
                 </button>
               </div>
               
               <div
                 className={
-                  "lg:flex flex-grow items-center bg-transparent" +
+                  "lg:flex flex-grow items-center" +
                   (menuOpen ? " flex bgcolor" : " hidden")
                 }
                 id="example-navbar-info"
               >
-                <ul className="flex flex-col lg:flex-row list-none lg:ml-auto my-0 pt-4 bg-transparent">
-                  <li className="nav-item bg-transparent">
+                <ul className="flex flex-col lg:flex-row list-none lg:ml-auto my-0 pt-4">
+                  <li className="nav-item">
                     <Link
                       className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-blue hover:opacity-75"
                       to="/Portfolio/"
@@ -43,7 +43,7 @@ const Header = ({ siteTitle }) => {
                       Portfolio
                     </Link>
                   </li>
-                  <li className="nav-item bg-transparent">
+                  <li className="nav-item">
                     <Link
                       className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-blue hover:opacity-75"
                       to="/Pricing/"
@@ -51,7 +51,7 @@ const Header = ({ siteTitle }) => {
                       Pricing
                     </Link>
                   </li>
-                  <li className="nav-item bg-transparent">
+                  <li className="nav-itemt">
                     <a
                       className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-blue hover:opacity-75"
                       href="#pablo"
@@ -59,7 +59,7 @@ const Header = ({ siteTitle }) => {
                       Blog
                     </a>
                   </li>
-                  <li className="nav-item bg-transparent">
+                  <li className="nav-item">
                     <Link
                       className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-blue hover:opacity-75"
                       to="/About/"
