d3fec36f177013a1e787fcae0dafafd168226098
Abdul Hamid Usman <Hazemidoo@gmail.com>
2020-12-30 19:38:47 +0100

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -48,14 +48,13 @@ const Layout = ({ children }) => {
 				<span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2.02c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm0 12.55l-5.992-4.57h11.983l-5.991 4.57zm0 1.288l-6-4.629v6.771h12v-6.771l-6 4.629z"/></svg></span>
 				<a className="hover:text-gray-900" href="mailto:baqi@gmail.com">baqi@gmail.com</a>
 			    </div>
-			    <p>
+			    <p className="m-0">
 			    01 3rd Avenue
 			    <br/>
 			    Gwarinpa Estate
 			    <br/>
 			    Abuja Nigeria
 			    </p>
-			    <div className="w-10 h-10"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg></ div>
 			    <p>Digital by <span className="text-gray-600 hover:text-gray-900">Haze</span></p>
 			</div>
 
