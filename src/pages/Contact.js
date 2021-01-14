2ac82be6e35ca411762acb9147eebdfe8b5461f9
Abdul Hamid Usman <Hazemidoo@gmail.com>
2021-01-14 09:47:44 -0800

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -42,7 +42,7 @@ const Contact = () => {
           <SEO title="Contact" />
           <section className="text-gray-600 body-font relative">
               <div className="absolute inset-0 bg-gray-300">
-                <iframe style={{filter: grayscale(1) contrast(1.2) opacity(0.4)}} marginheight="0" marginwidth="0" title="map" scrolling="no" src="https://www.google.com/maps/place/3rd+Ave,+Gwarinpa,+Abuja/@9.0968695,7.4075129,17z/data=!3m1!4b1!4m5!3m4!1s0x104e756c1f179b5b:0x45e76b2cd4962f33!8m2!3d9.0968695!4d7.4097016" width="100%" height="100%" frameborder="0"></iframe>
+                <iframe style={{filter: grayscale(1), contrast(1.2), opacity(0.4)}} marginheight="0" marginwidth="0" title="map" scrolling="no" src="https://www.google.com/maps/place/3rd+Ave,+Gwarinpa,+Abuja/@9.0968695,7.4075129,17z/data=!3m1!4b1!4m5!3m4!1s0x104e756c1f179b5b:0x45e76b2cd4962f33!8m2!3d9.0968695!4d7.4097016" width="100%" height="100%" frameborder="0"></iframe>
               </div>
               <div className="container px-5 py-24 mx-auto flex">
                 <div className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10" style={{boxShadow: "0 2px 30px 0 rgba(0,0,0,.63)"}} >
