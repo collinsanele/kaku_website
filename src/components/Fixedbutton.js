cbd60309cc9d0dcda610bcdcd6b7a8ce7ee846dc
Abdul Hamid Usman <Hazemidoo@gmail.com>
2021-02-15 21:14:00 +0100

    Update Fixedbutton.js

--- src/components/Fixedbutton.js
+++ src/components/Fixedbutton.js
@@ -17,7 +17,7 @@ const Fixedbutton = () => {
   // window.addEventListener('scroll', pop);
 
   return (
-    <div style={{minWidth:'50px', minHeight:'50px'}} className="blackbutton fixed w-15 h-15 rounded bg-black transition transform duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 bottom-10 right-10 border border-green-500 flex flex-col justify-center items-center focus:outline-none z-10" style={{boxShadow: "0 2px 10px 0 rgba(0,0,0,.63)"}}>
+    <div style={{minWidth:'44px', minHeight:'50px'}} className="blackbutton fixed w-11 h-11 rounded bg-black transition transform duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 bottom-10 right-10 border border-green-500 flex flex-col justify-center items-center focus:outline-none z-10" style={{boxShadow: "0 2px 10px 0 rgba(0,0,0,.63)"}}>
     <Link to="/Contact/"><img src={Speechbubble} alt="talk" className="filterg4 m-5"/></Link>
     </div>
   )
