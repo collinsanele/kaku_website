2c749ad2efa83aba48320a1029646083ae50e856
Abdulhamid Usman <Hazemidoo@gmail.com>
2021-04-21 17:17:49 +0100

    noto font for logo

--- src/components/header.js
+++ src/components/header.js
@@ -9,7 +9,7 @@ const Header = ({ siteTitle }) => {
     <nav class="shadow fixed bgcolor z-30 w-full">
       <div class=" mx-auto py-3 md:flex md:justify-between md:items-center relative">
         <div class="flex justify-between items-center">
-          <div class="flex justify-center items-center text-2xl ml-4 text-transparent bg-gradient-to-r bg-clip-text from-blue-500 to-green-500">
+          <div class="flex justify-center items-center noto text-2xl ml-4 text-transparent bg-gradient-to-r bg-clip-text from-blue-500 to-green-500">
             <Link to="/" class="">
               {siteTitle}
             </Link>
