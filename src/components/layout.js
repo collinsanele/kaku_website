d5deecea8800a02df22a28653852af67cb9e6ecc
Abdulhamid Usman <hazemidoo@gmail.com>
2021-05-23 16:17:53 +0100

    added aos

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,18 +5,22 @@
  * See: https://www.gatsbyjs.com/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import React, { useEffect } from 'react'
+import PropTypes from 'prop-types'
+import { useStaticQuery, graphql } from 'gatsby'
 
-import Header from "./header"
-import "./layout.css"
-import "fontsource-roboto"
-import "fontsource-open-sans"
-import "fontsource-noto-sans-jp"
-import Phone from "../images/Phone.svg"
-import Email from "../images/Email.svg"
-import Linkedin from "../images/Linkedin.svg"
+import Header from './header'
+import './layout.css'
+import 'fontsource-roboto'
+import 'fontsource-open-sans'
+import 'fontsource-noto-sans-jp'
+import Phone from '../images/Phone.svg'
+import Email from '../images/Email.svg'
+import Linkedin from '../images/Linkedin.svg'
+
+// animate on scroll
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -29,6 +33,12 @@ const Layout = ({ children }) => {
     }
   `)
 
+  useEffect(() => {
+    AOS.init({
+      duration: 1500
+    })
+  }, []);
+
   return (
     <>
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
@@ -36,123 +46,133 @@ const Layout = ({ children }) => {
         <main>{children}</main>
         {/* FOOTER */}
         <footer>
-          <div className="bg-gray-101 w-full text-black -mt-7 pt-7">
-            <div className="flex flex-wrap w-11/12 lg:w-8/12 mx-auto py-7">
-              <div className="w-full lg:w-4/12 px-4 ml-auto text-sm mb-7 md:mb-0">
-                <h3 className="inline text-4xl font-bold noto text-transparent bg-gradient-to-r bg-clip-text from-purple-874 via-purple-874 to-blue-600">
+          <div className='bg-gray-101 w-full text-black -mt-7 pt-7'>
+            <div className='flex flex-wrap w-11/12 lg:w-8/12 mx-auto py-7'>
+              <div className='w-full lg:w-4/12 px-4 ml-auto text-sm mb-7 md:mb-0'>
+                <h3
+                  className='inline text-4xl font-bold noto text-transparent bg-gradient-to-r bg-clip-text from-purple-874 via-purple-874 to-blue-600'
+                  data-aos={'fade-up'}
+                >
                   Kaku
                 </h3>
-                <div className="flex pt-12">
-                  <span className="h-6 pr-1">
-                    <img src={Phone} alt="Phone:" className=" w-4 h-4" />
+                <div className='flex pt-12'>
+                  <span className='h-6 pr-1'>
+                    <img src={Phone} alt='Phone:' className=' w-4 h-4' />
                   </span>
-                  <a className="hover:text-purple-874" href="tel:+2347012809187">
+                  <a
+                    className='hover:text-purple-874'
+                    href='tel:+2347012809187'
+                  >
                     +234 701 2809 187
                   </a>
                 </div>
-                <div className="flex">
-                  <span className="h-6 pr-1">
-                    <img src={Email} alt="Email:" className=" w-4 h-4" />
+                <div className='flex'>
+                  <span className='h-6 pr-1'>
+                    <img src={Email} alt='Email:' className=' w-4 h-4' />
                   </span>
                   <a
-                    className="hover:text-purple-874"
-                    href="mailto:admin@kaku.com.ng"
+                    className='hover:text-purple-874'
+                    href='mailto:admin@kaku.com.ng'
                   >
                     admin@kaku.com.ng
                   </a>
                 </div>
-                <p className="m-0">
+                <p className='m-0'>
                   3rd Avenue
                   <br />
                   Gwarinpa Estate
                   <br />
                   Abuja Nigeria.
                 </p>
-                <p >
-                  Digital by{" "}
+                <p>
+                  Digital by{' '}
                   <a
-                    className="text-gray-500 font-bold hover:text-green-500"
-                    href="https://hzmid.com"
+                    className='text-gray-500 font-bold hover:text-green-500'
+                    href='https://hzmid.com'
                   >
                     Haze
                   </a>
                 </p>
               </div>
 
-              <div className="w-full lg:w-4/12 px-4 ml-auto pb-7">
-                <span className="block noto uppercase text-xl font-bold mb-2">
+              <div className='w-full lg:w-4/12 px-4 ml-auto pb-7'>
+                <span className='block noto uppercase text-xl font-bold mb-2'>
                   Our Company
                 </span>
-                <ul className="list-unstyled">
+                <ul className='list-unstyled'>
                   <li>
-                    <a className="hover:text-purple-874 pb-1 text-sm" href="#">
+                    <a className='hover:text-purple-874 pb-1 text-sm' href='#'>
                       Home
                     </a>
                   </li>
                   <li>
-                    <a className="hover:text-purple-874 pb-1 text-sm" href="#">
+                    <a className='hover:text-purple-874 pb-1 text-sm' href='#'>
                       Blog
                     </a>
                   </li>
                   <li>
-                    <a className="hover:text-purple-874 pb-1 text-sm" href="#">
+                    <a className='hover:text-purple-874 pb-1 text-sm' href='#'>
                       Portfolio
                     </a>
                   </li>
                   <li>
-                    <a className="hover:text-purple-874 pb-1 text-sm" href="#">
+                    <a className='hover:text-purple-874 pb-1 text-sm' href='#'>
                       Pricing
                     </a>
                   </li>
                   <li>
-                    <a className="hover:text-purple-874 pb-1 text-sm" href="#">
+                    <a className='hover:text-purple-874 pb-1 text-sm' href='#'>
                       About Us
                     </a>
                   </li>
                   <li>
-                    <a className="hover:text-purple-874 pb-1 text-sm" href="#">
+                    <a className='hover:text-purple-874 pb-1 text-sm' href='#'>
                       Contact us
                     </a>
                   </li>
                   <li>
-                    <a className="hover:text-purple-874 pb-1 text-sm" href="#">
+                    <a className='hover:text-purple-874 pb-1 text-sm' href='#'>
                       Privacy policy
                     </a>
                   </li>
                   <li>
-                    <a className="hover:text-purple-874 pb-1 text-sm" href="#">
+                    <a className='hover:text-purple-874 pb-1 text-sm' href='#'>
                       Accessibility policy
                     </a>
                   </li>
                 </ul>
               </div>
 
-              <div className="w-full lg:w-4/12 px-4 ml-auto">
-                <span className="block noto uppercase text-xl font-bold mb-2">
+              <div className='w-full lg:w-4/12 px-4 ml-auto'>
+                <span className='block noto uppercase text-xl font-bold mb-2'>
                   Our Services
                 </span>
-                <ul className="list-unstyled">
-                  <li className="hover:text-purple-874 pb-1 text-sm">
+                <ul className='list-unstyled'>
+                  <li className='hover:text-purple-874 pb-1 text-sm'>
                     Articles and Blogs
                   </li>
-                  <li className="hover:text-purple-874 pb-1 text-sm">
+                  <li className='hover:text-purple-874 pb-1 text-sm'>
                     Advertorials
                   </li>
-                  <li className="hover:text-purple-874 pb-1 text-sm">
+                  <li className='hover:text-purple-874 pb-1 text-sm'>
                     Web and Email Copies
                   </li>
-                  <li className="hover:text-purple-874 pb-1 text-sm">Scripts</li>
-                  <li className="hover:text-purple-874 pb-1 text-sm">UX Writing</li>
-                  <li className="hover:text-purple-874 pb-1 text-sm">
+                  <li className='hover:text-purple-874 pb-1 text-sm'>
+                    Scripts
+                  </li>
+                  <li className='hover:text-purple-874 pb-1 text-sm'>
+                    UX Writing
+                  </li>
+                  <li className='hover:text-purple-874 pb-1 text-sm'>
                     Website Content
                   </li>
-                  <li className="hover:text-purple-874 pb-1 text-sm">
+                  <li className='hover:text-purple-874 pb-1 text-sm'>
                     Advertisement Strategies
                   </li>
-                  <li className="hover:text-purple-874 pb-1 text-sm">
+                  <li className='hover:text-purple-874 pb-1 text-sm'>
                     Media Content
                   </li>
-                  <li className="hover:text-purple-874 pb-1 text-sm">
+                  <li className='hover:text-purple-874 pb-1 text-sm'>
                     Competition Research
                   </li>
                 </ul>
@@ -160,16 +180,16 @@ const Layout = ({ children }) => {
             </div>
           </div>
 
-          <div className="w-full flex align-center justify-center bg-gray-101 pb-7 focus:outline-none">
-            <a href="https://www.linkedIn.com/company/kaku-ng">
+          <div className='w-full flex align-center justify-center bg-gray-101 pb-7 focus:outline-none'>
+            <a href='https://www.linkedIn.com/company/kaku-ng'>
               <img
                 src={Linkedin}
-                alt="Linkedin"
-                className=" focus:outline-none transition transform duration-500 ease-in-out hover:-translate-y-1 hover:scale-110"
+                alt='Linkedin'
+                className=' focus:outline-none transition transform duration-500 ease-in-out hover:-translate-y-1 hover:scale-110'
               />
             </a>
           </div>
-          <div className="w-full flex align-center justify-center bg-gray-101 text-xs text-black mb-5">
+          <div className='w-full flex align-center justify-center bg-gray-101 text-xs text-black mb-5'>
             <p>Copyright © 2021 KAKU Inc.</p>
           </div>
         </footer>
@@ -179,7 +199,7 @@ const Layout = ({ children }) => {
 }
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired
 }
 
 export default Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,7 @@ const IndexPage = () => {
 
   useEffect(() => {
     AOS.init({
-      duration: 2000
+      duration: 1500
     })
   }, [])
 
@@ -35,16 +35,16 @@ const IndexPage = () => {
           <div className='items-center md:flex'>
             <div className='w-full md:w-1/2'>
               <div className='max-w-lg mt-10 md:mt-0'>
-                <h1 className='noto text-2xl font-semibold text-purple-874 md:text-3xl'>
+                <h1 className='noto text-2xl font-semibold text-purple-874 md:text-3xl' data-aos={"fade-up"} data-aos-delay={"300"}>
                   A One-Stop Solution To All Your Brands' Contents Needs
                 </h1>
-                <p className='mt-4 text-gray-600 dark:text-gray-400'>
+                <p className='mt-4 text-gray-600 dark:text-gray-400' data-aos={"fade-up"} data-aos-delay={"400"}>
                   Brands need to stand out in today's digital space and we
                   create content that helps your brand connect with your ideal
                   audience. Its the perfect match!
                 </p>
                 <Link to='/Pricing/'>
-                  <button className='px-3 py-2 mt-8 text-sm font-medium text-white uppercase bg-gradient-to-r from-purple-874 via-purple-874 to-blue-600 rounded-md hover:from-black focus:outline-none focus:bg-black'>
+                  <button className='px-3 py-2 mt-8 text-sm font-medium text-white uppercase bg-gradient-to-r from-purple-874 via-purple-874 to-blue-600 rounded-md hover:from-black focus:outline-none focus:bg-black' data-aos={"fade-up"} data-aos-delay={"500"}>
                     Check our prices
                   </button>{' '}
                 </Link>
@@ -71,11 +71,11 @@ const IndexPage = () => {
               <div className='flex flex-col'>
                 <img src={content_m} alt='' className='w-32 self-center' />
 
-                <h2 className='mt-4 text-xl font-semibold text-purple-874'>
+                <h2 className='mt-4 text-xl font-semibold text-purple-874' data-aos={"fade-up"}>
                   Content Writing
                 </h2>
 
-                <p className='mt-2 text-gray-500 dark:text-gray-400'>
+                <p className='mt-2 text-gray-500 dark:text-gray-400'  data-aos={"fade-up"}>
                   What are your brand’s goals? More conversion, engagement,
                   exposure? You name it! We create highly-captivating content
                   that prompts action from your readers. Bringing you a step
@@ -86,11 +86,11 @@ const IndexPage = () => {
               <div className='flex flex-col'>
                 <img src={content_f} alt='' className='w-60 self-center' />
 
-                <h2 className='mt-4 text-xl font-semibold  text-purple-874'>
+                <h2 className='mt-4 text-xl font-semibold text-purple-874' data-aos={"fade-up"}>
                   Articles and Blog posts
                 </h2>
 
-                <p className='mt-2 text-gray-500 dark:text-gray-400'>
+                <p className='mt-2 text-gray-500 dark:text-gray-400' data-aos={"fade-up"}>
                   From your industry’s trending topics to informative how-to’s,
                   add value and connect with your target audience with articles
                   that have been optimized for search engines.
@@ -100,11 +100,11 @@ const IndexPage = () => {
               <div className='flex flex-col'>
                 <img src={groupchat} alt='' className='w-56 self-center' />
 
-                <h2 className='mt-4 text-xl font-semibold text-purple-874'>
+                <h2 className='mt-4 text-xl font-semibold text-purple-874' data-aos={"fade-up"}>
                   Technical Writing
                 </h2>
 
-                <p className='mt-2 text-gray-500 dark:text-gray-400'>
+                <p className='mt-2 text-gray-500 dark:text-gray-400' data-aos={"fade-up"}>
                   In need of who can creatively express all the boring technical
                   jargon? Our diverse industry specialists are at hand to create
                   content tailored to your niche.
@@ -118,11 +118,11 @@ const IndexPage = () => {
               <div className='flex flex-col'>
                 <img src={typewriter} alt='' className='w-56 self-center' />
 
-                <h2 className='mt-5 text-xl font-semibold text-purple-874'>
+                <h2 className='mt-5 text-xl font-semibold text-purple-874' data-aos={"fade-up"}>
                   UI & UX Writing
                 </h2>
 
-                <p className='mt-2 text-gray-500 dark:text-gray-400'>
+                <p className='mt-2 text-gray-500 dark:text-gray-400' data-aos={"fade-up"}>
                   Provide a memorable digital experience for your everyone that
                   visits your platform by ensuring your message is clear and
                   concise. Let our team of creatives infuse your brand’s persona
@@ -133,11 +133,11 @@ const IndexPage = () => {
               <div className='flex flex-col'>
                 <img src={podcast_f} alt='' className='w-60 self-center' />
 
-                <h2 className='mt-4 text-xl font-semibold text-purple-874'>
+                <h2 className='mt-4 text-xl font-semibold text-purple-874' data-aos={"fade-up"}>
                   Script Writing
                 </h2>
 
-                <p className='mt-2 text-gray-500 dark:text-gray-400'>
+                <p className='mt-2 text-gray-500 dark:text-gray-400' data-aos={"fade-up"}>
                   Getting started on your podcast or YouTube channel? or Running
                   a short ad campaign for your brand? Let us develop scripts
                   that engage and keep your listeners or viewers while at the
